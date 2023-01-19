bbe0095d54dbc155c39707ba177592e329e40f14
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-01-19 07:25:34 +0100

    test: enable test for io.local.fetchSockets() method
    The local flag is now taken in account since `socket.io@4.5.1`.
    
    Related: https://github.com/socketio/socket.io/commit/30430f0985f8e7c49394543d4c84913b6a15df60

--- test/index.ts
+++ test/index.ts
@@ -357,8 +357,7 @@ describe("@socket.io/mongodb-adapter", () => {
       expect(remoteSocket.rooms.size).to.eql(1);
     });
 
-    // TODO needs to be fixed in the `socket.io` package
-    it.skip("returns only local socket instances", async () => {
+    it("returns only local socket instances", async () => {
       const sockets = await servers[0].local.fetchSockets();
 
       expect(sockets).to.have.length(1);
