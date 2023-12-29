26ed5a8f62ed1a1822e64f23b3a687776d3fb939
Joël Luijmes <me@joell.dev>
2023-12-29 09:37:31 +0100

    fix(build): object is possibly null
    The return value of `findOneAndDelete` can be null, causing (modern) typescript to raise `TS2531: Object is possibly 'null'`

--- lib/index.ts
+++ lib/index.ts
@@ -821,7 +821,7 @@ export class MongoAdapter extends Adapter {
       return Promise.reject("error while fetching session");
     }
 
-    if (!results[0].value || !results[1]) {
+    if (!results[0]?.value || !results[1]) {
       return Promise.reject("session or offset not found");
     }
 
